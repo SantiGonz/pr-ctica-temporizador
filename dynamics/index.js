2a9c1d23ee80d98d07224a85a0579eeecf64c90d
Santiago Gonzalez <321090412@alumno.enp.unam.mx>
2022-05-25 20:45:34 -0500

    maquetado y modal ya desplegado

---
+++ dynamics/index.js
@@ -0,0 +1,6 @@
+const tiempo = document.getElementById("tiempo");
+const modal = document.getElementById("exampleModal");
+
+tiempo.addEventListener("click", ()=>{
+    modal.style.display = "block";
+})
